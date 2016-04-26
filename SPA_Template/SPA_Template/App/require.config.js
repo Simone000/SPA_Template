c3d1734f73111bfc0568d12976d852c96fc9e854
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-04-26 12:01:49 +0200

    jquery update

--- SPA_Template/SPA_Template/App/require.config.js
+++ SPA_Template/SPA_Template/App/require.config.js
@@ -3,7 +3,7 @@
     baseUrl: "/",
     paths: {
         "bootstrap": "Scripts/bootstrap",
-        "jquery": "Scripts/jquery-2.2.2",
+        "jquery": "Scripts/jquery-2.2.3",
         "blockUI": "Scripts/jquery.blockUI",
         "knockout": "Scripts/knockout-3.4.0",
         "knockoutgrids": "Scripts/knockout-grids",
