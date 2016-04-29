e561db1de87065e84c895fd8a83fac92e364e6ef
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-04-29 12:21:01 +0200

    Nuget out

---
+++ SPA_Template/SPA_TemplateHelpers/NugetOutput/content/App-generate/templates/ko-servergrid-t1/ko-servergrid-t1.js
@@ -0,0 +1,38 @@
+﻿define(["knockout", "text!./ko-servergrid-t1.html", "toastr", "api", "common", "knockoutgrids", "bootstrap"], function (ko, pageTemplate, toastr, api, common, knockoutgrids) {
+    function pageModel(params) {
+        var self = this;
+
+        self.prestazioniAziendaDaFatturares = ko.observableArray();
+
+        self.loadprestazioniAziendaDaFatturares = function (PageSize, CurrPage, SortBy, IsDesc, SearchBy, Search) {
+            function success(data) {
+                self.prestazioniAziendaDaFatturares(ko.utils.arrayMap(data, function (item) {
+                    return new common.PrestazioniAziendaDaFatturare(item);
+                }));
+            };
+            function error(jqXHR, desc) {
+                if (jqXHR["status"] == 401) {
+                    //window.location = "/#/account/login";
+                    return;
+                }
+                //comment if not using validation-summary-errors
+                toastr["error"](desc, "Errore!");
+            };
+            api.GetPrestazioniAttiveDaFatturarePaged($('#div_prestazioniAziendaDaFatturares'), success, error, PageSize, CurrPage, SortBy, IsDesc, SearchBy, Search);
+        };
+
+        self.prestazioniAziendaDaFatturaresPaginate = ko.observable(
+            new knockoutgrids.ServerGrid(self.prestazioniAziendaDaFatturares, 10, 'NomeOrganizzazione',
+                function (PageSize, CurrPage, SortBy, IsDesc, SearchBy, Search) { //OnChange
+                    console.log("OnChange" + " PageSize:" + PageSize + " CurrPage:" + CurrPage + " SortBy:" + SortBy + " IsDesc:" + IsDesc + " SearchBy:" + SearchBy + " Search:" + Search);
+                    if (!Search)
+                        Search = '';
+                    self.loadprestazioniAziendaDaFatturares(PageSize, CurrPage, SortBy, IsDesc, SearchBy, Search);
+                })
+        );
+        self.loadprestazioniAziendaDaFatturares(10, 0, 'NomeOrganizzazione', true, 'NomeOrganizzazione', '');
+
+        return self;
+    }
+    return { viewModel: pageModel, template: pageTemplate };
+});
