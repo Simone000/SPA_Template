2d779401d4ce8c8e1d16360c393f3ade17b9768c
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-06-23 12:09:55 +0200

    comments

--- SPA_Template/SPA_Template/CustomResponseHandler.cs
+++ SPA_Template/SPA_Template/CustomResponseHandler.cs
@@ -12,18 +12,8 @@ namespace SPA_Template
     //config.MessageHandlers.Replace(new CustomResponseHandler());
     public class CustomResponseHandler : DelegatingHandler
     {
-        //todo: parametri da web config per log request e response
-
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            //request
-            if(request != null)
-            {
-                //todo: copiare da global.asax
-
-                //Trace.TraceInformation("HTTP {0}, Url: {1}, parametri: {2}, Utente: {3}, IP: {4}", tipoRichiesta, url, parametri, username, ip);
-            }
-
             var response = await base.SendAsync(request, cancellationToken);
 
             if (response != null && response.Content != null)
