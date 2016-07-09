0bbd435165e82d8a7b4ae7e89758717c4f98a6bf
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-07-09 23:49:22 +0200

    template helpers

---
+++ SPA_Template/SPA_TemplateHelpers/App/account/page-login/page-login.js
@@ -0,0 +1,27 @@
+﻿define(["knockout", "text!./page-login.html", "toastr", "api", "bootstrap"], function (ko, AnyLoginTemplate, toastr, api) {
+    function AnyLogin(params) {
+        var self = this;
+
+        self.email = ko.observable('assistenzaclienti@cosi.it');
+        self.password = ko.observable('assistenzaclienti..');
+
+        self.login = function () {
+            function success(data) {
+                window.location.replace("");
+            };
+            function error(jqXHR, desc) {
+                //toastr["error"](desc, "Errore!");
+            };
+            api.Login($('#div_login'), success, error, self.email(), self.password(), null);
+        };
+
+        self.enter = function (data, event) {
+            if (event.keyCode == 13)
+                self.login();
+            return true;
+        };
+
+        return self;
+    }
+    return { viewModel: AnyLogin, template: AnyLoginTemplate };
+});
