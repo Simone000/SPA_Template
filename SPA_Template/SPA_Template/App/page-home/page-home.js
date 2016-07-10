133d7a32f6704450f068d62c77853bfc4715a37d
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-07-10 15:14:27 +0200

    mini fix

--- SPA_Template/SPA_Template/App/page-home/page-home.js
+++ SPA_Template/SPA_Template/App/page-home/page-home.js
@@ -1,4 +1,4 @@
-﻿//define(["knockout", "text!./page-empty.html", "toastr", "api", "common", "knockoutgrids", "bootstrap"], function (ko, pageTemplate, toastr, api, common, knockoutgrids) {
+﻿//define(["knockout", "text!./page-home.html", "toastr", "api", "common", "knockoutgrids", "bootstrap"], function (ko, pageTemplate, toastr, api, common, knockoutgrids) {
 define(["knockout", "text!./page-home.html", "toastr", "api", "bootstrap"], function (ko, pageTemplate, toastr, api) {
     function pageModel(params) {
         var self = this;
