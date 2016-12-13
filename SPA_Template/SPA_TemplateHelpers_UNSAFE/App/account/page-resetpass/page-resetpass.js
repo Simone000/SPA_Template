84edade7cecc05b410b4e26b05a7e68b37f8b668
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-12-13 13:12:56 +0100

    reset pass encoding

--- SPA_Template/SPA_TemplateHelpers_UNSAFE/App/account/page-resetpass/page-resetpass.js
+++ SPA_Template/SPA_TemplateHelpers_UNSAFE/App/account/page-resetpass/page-resetpass.js
@@ -4,19 +4,20 @@
 
         self.userid = params.userid;
         self.code = params.code;
+        self.encodedCode = encodeURIComponent(params.code);
 
         self.password = ko.observable();
         self.confirmPassword = ko.observable();
 
         self.resetPass = function () {
             function success(data) {
-                toastr["success"]("Email di recupero inviata");
+                toastr["success"]("Password Cambiata!");
                 window.location = "/#/account/login";
             };
             function error(jqXHR, desc) {
                 toastr["error"](desc, "Errore!");
             };
-            api.ResetPassword($('#div_resetPass'), success, error, self.userid, self.code, self.password(), self.confirmPassword());
+            api.ResetPassword($('#div_resetPass'), success, error, self.userid, self.encodedCode, self.password(), self.confirmPassword());
         };
 
         return self;
