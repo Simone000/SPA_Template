250c070caff65a1d5c797a5b95910704196e0243
simone <simone.invernizzi@hotmail.com>
2017-01-25 21:57:48 +0100

    Spa settings

---
+++ SPA_Template/SPA_Template/Controllers/TempController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SPA_Template.Controllers
+{
+    public class TempController : ApiController
+    {
+    }
+}
