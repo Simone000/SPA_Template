c219f394db566d0cd5ee76df6ac8dd44efbd1bb0
simone <simone.invernizzi@hotmail.com>
2017-11-20 14:36:43 +0100

    remove unused datepicker

--- SPA_Template/SPA_TemplateHelpers_UNSAFE/App/account/page-user/page-user.js
+++ SPA_Template/SPA_TemplateHelpers_UNSAFE/App/account/page-user/page-user.js
@@ -1,4 +1,4 @@
-﻿define(["knockout", "text!./page-user.html", "toastr", "api", "common", "knockoutgrids", "bootstrap", "datepicker", "datepickerITA"], function (ko, pageTemplate, toastr, api, common, knockoutgrids) {
+﻿define(["knockout", "text!./page-user.html", "toastr", "api", "common", "knockoutgrids", "bootstrap"], function (ko, pageTemplate, toastr, api, common, knockoutgrids) {
     function pageModel(params) {
         var self = this;
 
