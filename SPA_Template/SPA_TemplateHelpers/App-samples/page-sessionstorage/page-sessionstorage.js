aa14a70eb905ac1002e77e5edbc84d71e6ffca4e
Simone Invernizzi <simone.invernizzi@hotmail.com>
2018-02-20 17:26:07 +0100

    session storage sample

---
+++ SPA_Template/SPA_TemplateHelpers/App-samples/page-sessionstorage/page-sessionstorage.js
@@ -0,0 +1,25 @@
+﻿//define(["knockout", "text!./page-sessionstorage.html", "toastr", "api", "common", "knockoutgrids", "bootstrap"], function (ko, pageTemplate, toastr, api, common, knockoutgrids) {
+define(["knockout", "text!./page-sessionstorage.html", "toastr", "api", "common", "bootstrap"], function (ko, pageTemplate, toastr, api, common) {
+    function pageModel(params) {
+        var self = this;
+
+        //saving a boolean preference
+        self.thingIsHidden = ko.observable(false);
+        if (typeof (Storage) !== "undefined") {
+            //with a saved thing => I do apply it (boolean are still saved as strings)
+            if (sessionStorage.thingIsHidden) {
+                self.aziendaGridHidden(sessionStorage.thingIsHidden === "true");
+            }
+        }
+        self.changeGridVisibility = function () {
+            self.thingIsHidden(!self.thingIsHidden());
+
+            if (typeof (Storage) !== "undefined") {
+                sessionStorage.thingIsHidden = self.thingIsHidden();
+            }
+        };
+
+        return self;
+    }
+    return { viewModel: pageModel, template: pageTemplate };
+});
