66f383cc09ffa0f7288abd3b1ebad948ae234f04
simone <simone.invernizzi@hotmail.com>
2018-12-10 14:40:33 +0100

    nuget upd

--- SPA_Template/SpaTemplateCustomHelpers/Controllers/HomeController.cs
+++ SPA_Template/SpaTemplateCustomHelpers/Controllers/HomeController.cs
@@ -10,9 +10,7 @@ namespace SpaTemplateCustomHelpers.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
-            return View();
+            return RedirectToAction("Index", "Help");
         }
     }
 }
